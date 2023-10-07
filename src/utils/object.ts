dc6e21aabced5b7d6e064401a526446b7bf8339a
panmqd1 <36785224+panmqd1@users.noreply.github.com>
2023-10-07 09:59:17 +0800

    feat: reg; validate;

--- src/utils/object.ts
+++ src/utils/object.ts
@@ -3,7 +3,7 @@ export const keysOf = <T extends object>(obj: T) => {
 };
 
 /**
- * 
+ *
  * @param obj 需要被赋值的对象
  * @param data 数据来源对象
  */
@@ -12,3 +12,23 @@ export const setObjectValueFromData = <T extends object>(obj: T, data: any) => {
     obj[key] = data?.[key];
   }
 };
+
+/**
+ * 从props配置中获取默认值
+ * @param props
+ * @param overrideProps
+ * @returns
+ */
+export function getDefaultFromProps<T = Record<string, any>>(
+  props: Record<string, any>,
+  overrideProps: T
+): T | Record<string, any> {
+  const defaults = Object.entries(props).reduce((temp, [key, value]) => {
+    temp[key] = value?.default;
+    return temp;
+  }, {} as any);
+  return {
+    ...defaults,
+    ...overrideProps,
+  };
+}
--- src/utils/validate.ts
+++ src/utils/validate.ts
@@ -1,1 +1,14 @@
-export default {}
+const regExps = {
+  mobile: /^1(3|4|5|6|7|8|9)\d{9}$/,
+  mobile2: /^1\d{10}$/,
+  email:
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+  // 至少8-16个字符，至少1个大写字母，1个小写字母和1个数字，其他可以是任意字符
+  password: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[^]{8,16}$/,
+};
+
+export const validPassword = (value: string) => {
+  return regExps.password.test(value);
+};
+
+export default regExps;
